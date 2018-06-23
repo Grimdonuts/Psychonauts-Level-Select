c3afc8c4c048284dae911c238fe33005ca1e3a43
Grimdonuts <Icescythe@hotmail.com>
2018-06-23 00:16:59 -0500

    combobox binding

--- LevelSelect/UI/LevelSelectSettings.xaml.cs
+++ LevelSelect/UI/LevelSelectSettings.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LevelSelect.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,31 @@ namespace LevelSelect.UI
         public LevelSelectSettings()
         {
             InitializeComponent();
+
+            comboBox1.ItemsSource = LevelSelectModel.Levels.Values;
+            comboBox1.SelectedIndex = 0;
+            comboBox2.ItemsSource = LevelSelectModel.Levels.Values;
+            comboBox2.SelectedIndex = 0;
+            comboBox3.ItemsSource = LevelSelectModel.Levels.Values;
+            comboBox3.SelectedIndex = 0;
+            comboBox4.ItemsSource = LevelSelectModel.Levels.Values;
+            comboBox4.SelectedIndex = 0;
+            comboBox5.ItemsSource = LevelSelectModel.Levels.Values;
+            comboBox5.SelectedIndex = 0;
+            comboBox6.ItemsSource = LevelSelectModel.Levels.Values;
+            comboBox6.SelectedIndex = 0;
+            comboBox7.ItemsSource = LevelSelectModel.Levels.Values;
+            comboBox7.SelectedIndex = 0;
+            comboBox8.ItemsSource = LevelSelectModel.Levels.Values;
+            comboBox8.SelectedIndex = 0;
+            comboBox9.ItemsSource = LevelSelectModel.Levels.Values;
+            comboBox9.SelectedIndex = 0;
+            comboBox10.ItemsSource = LevelSelectModel.Levels.Values;
+            comboBox10.SelectedIndex = 0;
+            comboBox11.ItemsSource = LevelSelectModel.Levels.Values;
+            comboBox11.SelectedIndex = 0;
+            comboBox12.ItemsSource = LevelSelectModel.Levels.Values;
+            comboBox12.SelectedIndex = 0;
         }
     }
 }
